5c0d79529e97dda932e9a16b9d63a96875715c49
Eduardo Borsato Reinert <eduardoreinert19@gmail.com>
2025-04-04 14:43:32 -0300

    Create criar_tab7.sql

---
+++ sql/criar_tab7.sql
@@ -0,0 +1,7 @@
+create table pessoa (
+	pessoaID integer not null,
+    nome varchar(255) not null,
+    endereco varchar(255) not null,
+    telefone varchar(255) not null,
+    cidade varchar(255) not null
+);pessoa
